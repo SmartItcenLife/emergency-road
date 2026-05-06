33d01d6a32f837fd326b98e3de8a001143fa4491
7888mk0915@gmail.com <7888mk0915@gmail.com>
2026-05-06 20:06:06 +0900

    [feat #10] : 중증질환 전체 요소 코드

--- emergencyroad/src/main/java/com/itcen/emergencyroad/hospital/entity/SrsillDTO.java
+++ emergencyroad/src/main/java/com/itcen/emergencyroad/hospital/entity/SrsillDTO.java
@@ -3,32 +3,32 @@ package com.itcen.emergencyroad.hospital.entity;
 public class SrsillDTO {
 
     // 일반
-    private String mkioskty1; // 심근경색
-    private String mkioskty2; // 뇌경색
-    private String mkioskty3; // 거미막하 출혈
-    private String mkioskty4; // 거미막하출혈 외
-    private String mkioskty5; // 대동맥응급_흉부
-    private String mkioskty6; // 대동맥응급_복부
-    private String mkioskty23; // 응급투석
-    private String mkioskty24; // 폐쇄병동입원
-    private String mkioskty11;// 응급 내시경-성인위장관
-    private String mkioskty13;// 응급내시경-성인 기관지
-    private String mkioskty19;// 중증화상-전문치료
-    private String mkioskty26;// 영상의학혈관중재-성인
+    private String MKioskTy1; // 심근경색
+    private String MKioskTy2; // 뇌경색
+    private String MKioskTy3; // 거미막하 출혈
+    private String MKioskTy4; // 거미막하출혈 외
+    private String MKioskTy5; // 대동맥응급_흉부
+    private String MKioskTy6; // 대동맥응급_복부
+    private String MKioskTy23; // 응급투석
+    private String MKioskTy24; // 폐쇄병동입원
+    private String MKioskTy11;// 응급 내시경-성인위장관
+    private String MKioskTy13;// 응급내시경-성인 기관지
+    private String MKioskTy19;// 중증화상-전문치료
+    private String MKioskTy26;// 영상의학혈관중재-성인
 
     // 임산부
-    private String mkioskty22; // 응급투석
-    //private String mkioskty15; // 저체중출생아 집중치료
-    private String mkioskty16; // 산부인과응급 분만
-    private String mkioskty17; // 산부인과응급 산과수술
-    private String mkioskty18; // 산부인과응급 부인과수술
+    private String MKioskTy22; // 응급투석
+    //private String MKioskTy15; // 저체중출생아 집중치료
+    private String MKioskTy16; // 산부인과응급 분만
+    private String MKioskTy17; // 산부인과응급 산과수술
+    private String MKioskTy18; // 산부인과응급 부인과수술
 
     // 소아
-    private String mkioskty10; // 장충첩/폐색_영유아
-    private String mkioskty12; // 응급내시경_영유아 위장관
-    private String mkioskty14; // 응급내시경_영유아_기관지
-    private String mkioskty15; // 저체중 출산아
-    private String mkioskty27; // 영상의학혈관중재_영유아
+    private String MKioskTy10; // 장충첩/폐색_영유아
+    private String MKioskTy12; // 응급내시경_영유아 위장관
+    private String MKioskTy14; // 응급내시경_영유아_기관지
+    private String MKioskTy15; // 저체중 출산아
+    private String MKioskTy27; // 영상의학혈관중재_영유아
 
 
 }
