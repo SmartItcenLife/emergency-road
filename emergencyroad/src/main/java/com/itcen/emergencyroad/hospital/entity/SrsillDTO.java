7d9127a2bfa977ec84831624288499b465e2eb43
7888mk0915@gmail.com <7888mk0915@gmail.com>
2026-05-06 19:39:29 +0900

    [feat/#10] : SrsillDTO 중증 질환 전체(일반/산모/소아)

---
+++ emergencyroad/src/main/java/com/itcen/emergencyroad/hospital/entity/SrsillDTO.java
@@ -0,0 +1,33 @@
+package com.itcen.emergencyroad.hospital.entity;
+
+public class SrsillDTO {
+    // 일반
+    private String mkioskty1; // 심근경색
+    private String mkioskty2; // 뇌경색
+    private String mkioskty3; // 거미막하 출혈
+    private String mkioskty4; // 거미막하출혈 외
+    private String mkioskty5; // 대동맥응급_흉부
+    private String mkioskty6; // 대동맥응급_복부
+    private String mkioskty23; // 응급투석
+    private String mkioskty24; // 폐쇄병동입원
+    private String mkioskty11;// 응급 내시경-성인위장관
+    private String mkioskty13;// 응급내시경-성인 기관지
+    private String mkioskty19;// 중증화상-전문치료
+    private String mkioskty26;// 영상의학혈관중재-성인
+
+    // 임산부
+    private String mkioskty22; // 응급투석
+    //private String mkioskty15; // 저체중출생아 집중치료
+    private String mkioskty16; // 산부인과응급 분만
+    private String mkioskty17; // 산부인과응급 산과수술
+    private String mkioskty18; // 산부인과응급 부인과수술
+
+    // 소아
+    private String mkioskty10; // 장충첩/폐색_영유아
+    private String mkioskty12; // 응급내시경_영유아 위장관
+    private String mkioskty14; // 응급내시경_영유아_기관지
+    private String mkioskty15; // 저체중 출산아
+    private String mkioskty27; // 영상의학혈관중재_영유아
+
+
+}
