9414a65736c85e977a50d70660f446b72d9e9663
me_nyong <gjalsdud1030@naver.com>
2026-05-12 12:07:55 +0900

    [feat/#54] PregnantHospitalProjection 조회 Repository 추가

--- emergencyroad/src/main/java/com/itcen/emergencyroad/hospital/repository/HospitalRepository.java
+++ emergencyroad/src/main/java/com/itcen/emergencyroad/hospital/repository/HospitalRepository.java
@@ -1,6 +1,8 @@
 package com.itcen.emergencyroad.hospital.repository;
 
 import com.itcen.emergencyroad.hospital.entity.Hospital;
+import com.itcen.emergencyroad.recommend.dto.projection.PregnantHospitalProjection;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
 import java.util.List;
@@ -10,4 +12,22 @@ public interface HospitalRepository extends JpaRepositoryImplementation<Hospital
     Optional<Hospital> findByHpid(String hpid);
     //여러 HPID를 한 번에 조회
     List<Hospital> findAllByHpidIn(java.util.Collection<String> hpids);
+
+
+    //필요한 엔티티들을 직접 조인해서 가져오는 쿼리
+    @Query("""
+            SELECT h as hospital,
+                   hd as detail,
+                   s as score,
+                   p as pregnant,
+                   pr as realtime,
+                   ps as standard
+            FROM Hospital h
+            LEFT JOIN HospitalDetail hd ON hd.hospital = h
+            LEFT JOIN HospitalScore s ON s.hospital = h
+            LEFT JOIN Pregnant p ON p.hospital = h
+            LEFT JOIN PregnantRealtime pr ON pr.hospital = h
+            LEFT JOIN PregnantStandard ps ON ps.hospital = h
+            """)
+    List<PregnantHospitalProjection> findAllHospitalPregnantData();
 }
